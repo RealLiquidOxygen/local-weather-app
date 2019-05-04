663bb8e33056ddb86f28cbfb8f728ee22bf6ff93
Ayodele Owoyele <dskyisnotdlimit@hotmail.com>
2019-05-03 22:34:40 -0500

    Upgraded to Angular 7
    Trying to add materials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div style="text-align: center">
+      <mat-toolbar>
+        LocalCast Weather
+      </mat-toolbar>
+      <div>Your city, your forecast, right now!</div>
+      <h2>Current Weather</h2>
+      <app-current-weather></app-current-weather>
+    </div>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HttpClientModule } from '@angular/common/http'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
 import { WeatherService } from './weather/weather.service'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './material.module'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    FlexLayoutModule,
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MaterialModule,
+  ],
   providers: [WeatherService],
   bootstrap: [AppComponent],
 })
